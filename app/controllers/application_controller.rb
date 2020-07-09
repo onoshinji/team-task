d44dd7ad2853b6e8ff8306d806a29394ae06d82d
oldrookie-engineer <paburofu0023@icloud.com>
2020-07-09 10:32:04 +0900

    blog機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  # def authenticate_user
+  #   # if current_user == nil
+  #   #   flash[:notice] = 'ログインしていません！'
+  #   #   redirect_to new_session_path
+  #   # end
+  # end
 end
